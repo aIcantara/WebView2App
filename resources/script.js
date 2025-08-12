e8d95b5b7d2f6da53692bdd05472bfdcc80637d0
aIcantara <152627480+aIcantara@users.noreply.github.com>
2025-08-12 10:31:47 +0300

    WebView2App

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,14 @@
 import { engine } from "res://js/engine.js"
 
 document.addEventListener("DOMContentLoaded", () => {
+    // block drag images
+    document.addEventListener("dragstart", (event) => {
+        if (event.target.tagName === "IMG")
+            event.preventDefault();
+    });
+
     document.querySelector(".header-controls-minimize").onclick = () => engine.app.minimize();
     document.querySelector(".header-controls-exit").onclick = () => engine.app.exit();
-    
+
     document.querySelector(".hello").onclick = () => engine.test.hello("World").then(response => alert(response));
 });
