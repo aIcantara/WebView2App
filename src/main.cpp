6f2dd74a3ce510096b7b064a9751ea8d416ed70a
aIcantara <152627480+aIcantara@users.noreply.github.com>
2025-08-12 06:33:11 +0300

    WebView2App

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "ui/window.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hInstancePrevious, LPSTR commandLine, int show)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     CWindow window("WebView2App", { 700, 500 });
 
